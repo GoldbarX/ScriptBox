57e16916b84af6fe09cf406e3b384ef8966682fa
EKid <159160965+TinyTosha@users.noreply.github.com>
2025-01-31 19:24:50 +0300

    For update site

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// ScriptBox by TinyTosha
 // Data
 const scripts = [
     { Name: "⭐ | Infinite Yield", Script: "loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()", Image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fofficial-jjsploit.com%2Fwp-content%2Fuploads%2F2023%2F05%2FInfinite-Yield-Free-Download.webp&f=1&nofb=1&ipt=85da040fdea573fb8cf4c020d0118fcd229e22dac4c2eb58eaba177f41c11ba2&ipo=images" },
